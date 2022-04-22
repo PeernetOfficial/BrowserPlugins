41c393318d4162a4b6e3985d165f3c3192f8f06e
Krzysztof Skubała <Krzysztof.Skubala@cshark.com>
2022-04-22 14:37:47 +0200

    Implement WordViewer [General documents Viewer]

--- Peernet.Browser.Plugins.WordViewer/Services/WordViewerService.cs
+++ Peernet.Browser.Plugins.WordViewer/Services/WordViewerService.cs
@@ -1,17 +1,17 @@
-﻿using Peernet.Browser.Plugins.ExcelViewer.ViewModels;
-using Peernet.Browser.Plugins.ExcelViewer.ViewModels.Parameters;
+﻿using Peernet.Browser.Plugins.WordViewer.ViewModels;
+using Peernet.Browser.Plugins.WordViewer.ViewModels.Parameters;
 using Peernet.SDK.Client.Clients;
 using Peernet.SDK.Models.Domain.Common;
 using Peernet.SDK.Models.Plugins;
 using System.Threading.Tasks;
 
-namespace Peernet.Browser.Plugins.ExcelViewer.Services
+namespace Peernet.Browser.Plugins.WordViewer.Services
 {
-    public class ExcelViewerService : IPlayButtonPlug
+    public class WordViewerService : IPlayButtonPlug
     {
         private readonly IFileClient fileClient;
 
-        public ExcelViewerService(IFileClient fileClient)
+        public WordViewerService(IFileClient fileClient)
         {
             this.fileClient = fileClient;
         }
@@ -19,15 +19,15 @@ namespace Peernet.Browser.Plugins.ExcelViewer.Services
         // Open Window on Execute
         public async Task Execute(ApiFile file)
         {
-            var viewModel = new ExcelViewerViewModel();
+            var viewModel = new WordViewerViewModel();
             var stream = await fileClient.Read(file);
-            await viewModel.Prepare(new ExcelViewerViewModelParameter { FileStream = stream, Title = file.Name });
-            new ExcelViewerWindow(viewModel).Show();
+            await viewModel.Prepare(new WordViewerViewModelParameter { FileStream = stream, Title = file.Name });
+            new WordViewerWindow(viewModel).Show();
         }
 
         public bool IsSupported(ApiFile file)
         {
-            if (file?.Format is HighLevelFileType.Excel)
+            if (file?.Format is HighLevelFileType.Word)
             {
                 return true;
             }
---
+++ Peernet.Browser.Plugins.WordViewer/WordViewerPlugin.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Peernet.Browser.Plugins.WordViewer.Services;
+using Peernet.SDK.Models.Plugins;
+
+namespace Peernet.Browser.Plugins.WordViewer
+{
+    public class WordViewerPlugin : IPlugin
+    {
+        public void Load(ServiceCollection services)
+        {
+            services.AddSingleton<IPlayButtonPlug, WordViewerService>();
+        }
+    }
+}
